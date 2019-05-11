4766411c5ab2e76c43f767d4fb703b392a7a2d0d
OmkarGosavi <gosaviomkar40@gmail.com>
2019-05-12 03:19:25 +0530

    broser -modal

--- main.js
+++ main.js
@@ -1,22 +1,26 @@
-$(document).ready(function(){
-
-    /* Get browser */
-    $.browser.chrome = /chrome/.test(navigator.userAgent.toLowerCase());
-    
-    /* Detect Chrome */
-    if($.browser.chrome){
-        /* Do something for Chrome at this point */
-        alert("You are using Chrome!");
-        
-        /* Finally, if it is Chrome then jQuery thinks it's 
-           Safari so we have to tell it isn't */
+$(document).ready(function () {
+    if ($.browser.webkit) {
+       
+        $("#modal-text").text(" Thank you for downloading, You are using Chrome!!");
         $.browser.safari = false;
-    }
-    
-    /* Detect Safari */
-    if($.browser.safari){
+
+    } else if ($.browser.safari) {
         /* Do something for Safari */
-        alert("You are using Safari!");
+       
+        $("#modal-text").text(" Thank you for downloading, You are using Safari!!");
+
+    } else if ($.browser.mozilla) {
+       
+        $("#modal-text").text(" Thank you for downloading, You are using Firefox!!");
+        
+    } else if ($.browser.msedge) {
+
+        $("#modal-text").text(" Thank you for downloading ,You are using Edge !!");
+
+    } else {
+        $("#modal-text").text("Sorry,Browser Not Supported");
+
     }
-    
-    });
+   
+
+});
